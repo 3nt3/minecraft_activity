d031ecfa0ec51f050641a2e686a836bcf532a4ca
3nt3 <gott@3nt3.de>
2021-02-21 14:14:50 +0100

    add markers

--- minecraft_activity.py
+++ minecraft_activity.py
@@ -87,6 +87,7 @@ i = 0
 for player_name in player_data.keys():
     for entry in player_data[player_name]:
         if entry[0] is None or entry[1] is None:
+            print("incomplete entry")
             continue
 
         entry[0] = time.mktime(datetime.datetime.strptime(
@@ -98,13 +99,13 @@ for player_name in player_data.keys():
         # plot
         if not player_colors.get(player_name):
             lines = plt.plot(entry, [i, i], linewidth=5,
-                             solid_capstyle='round', label=player_name)
+                             solid_capstyle='round', label=player_name, marker='x', markersize=10, markeredgecolor='black')
             player_colors[player_name] = lines[0].get_color()
             plt.legend()
 
         else:
             plt.plot(entry, [i, i], linewidth=5,
-                     solid_capstyle='round', color=player_colors[player_name])
+                     solid_capstyle='round', color=player_colors[player_name], marker='x', markersize=10, markeredgecolor='black')
 
     i += 1
 
